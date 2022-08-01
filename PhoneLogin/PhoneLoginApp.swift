4fd43adc88c962d782176743da155fc356b4ec76
Ochanyoro <wadaoo0223@icloud.com>
2022-08-01 22:06:35 +0900

    Initial Commit

---
+++ PhoneLogin/PhoneLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneLoginApp.swift
+//  PhoneLogin
+//
+//  Created by 大和田一裕 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct PhoneLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
