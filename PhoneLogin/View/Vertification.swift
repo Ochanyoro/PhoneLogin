7aaa6a701d891cf29758e4817b1e5f0f11e2ccc8
Ochanyoro <wadaoo0223@icloud.com>
2022-08-01 22:11:00 +0900

    [create] Certification

---
+++ PhoneLogin/View/Vertification.swift
@@ -0,0 +1,25 @@
+//
+//  Vertification.swift
+//  PhoneLogin
+//
+//  Created by 大和田一裕 on 2022/08/01.
+//
+
+import SwiftUI
+
+struct Vertification: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("認証")
+    }
+}
+
+struct Vertification_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
